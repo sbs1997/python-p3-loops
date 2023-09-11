b87221b962e24489add853639cbb974545f57209
Sky Bauerschmidt Sweeney <dbauerschmidtsweeney@gmail.com>
2023-09-11 14:37:09 -0600

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
+# happy_new_year()
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    result = []
+    for num in int_list:
+        result.append(num**2)
+    return result
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    x = 1
+    while x<=100:
+        if not x%3:
+            if not x%5:
+                print("FizzBuzz")
+            else: print("Fizz")
+        elif not x%5:
+            print("Buzz")
+        else: print(x)
+        x += 1 
+fizzbuzz()
